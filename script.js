c244da01125e3b9673feb77463c9e52db16afa46
Mkups1 <m.kupilik1@share.epsb.ca>
2022-03-21 13:24:19 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// draw sunrise
+let cnv = document.getElementById("canvas")
+let ctx = cnv.getContext("2d")
+cnv.width = 400
+cnv.height = 400
+
+let clothCloudImg = document.getElementById("cloth-cloud-img")
+
+ctx.fillStyle = "blue"
+ctx.fillRect(0, 0, 400, 300)
+
+ctx.fillStyle = "red"
+ctx.beginPath()
+ctx.arc(200, 300, 25, 0, 2 * Math.PI)
+ctx.fill()
+
+ctx.fillStyle = "green"
+ctx.fillRect(0, 300, 400, 100)
+
+
+
+
