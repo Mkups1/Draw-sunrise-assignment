3bde86a57d743af633c856b57e82db71a4db4992
Mkups1 <m.kupilik1@share.epsb.ca>
2022-03-24 12:49:04 -0600

    final commit

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ cnv.width = 400
 cnv.height = 400
 
 let clothCloudImg = document.getElementById("cloth-cloud-img")
+let cloudX = 180
+let cloudX2 = 140
+let sunY = 300
+let sunColour = 0
+let sunRadius = 25
+
 
 ctx.fillStyle = "blue"
 ctx.fillRect(0, 0, 400, 300)
@@ -17,6 +23,33 @@ ctx.fill()
 ctx.fillStyle = "green"
 ctx.fillRect(0, 300, 400, 100)
 
-ctx.drawImage(clothCloudImg, 140, 100)
-ctx.drawImage(clothCloudImg, 180, 80)
+
+requestAnimationFrame(loop)
+function loop(){
+    cloudX++
+    cloudX2--
+    sunY--
+    sunColour++
+    sunRadius += 0.1
+
+    ctx.fillStyle ="blue"
+    ctx.fillRect(0, 0, 400, 300)
+    
+    ctx.fillStyle = "green"
+    ctx.fillRect(0, 300, 400, 100)
+
+    ctx.drawImage(clothCloudImg, cloudX2, 100)
+    ctx.drawImage(clothCloudImg, cloudX, 80)
+
+    ctx.fillStyle = "rgb(255," + sunColour + ",0)"
+    ctx.beginPath()
+    ctx.arc(200, sunY, sunRadius, 0, 2 * Math.PI)
+    ctx.fill()
+
+    ctx.fillStyle = "green"
+    ctx.fillRect(0, 300, 400, 100)
+    
+    requestAnimationFrame(loop)
+}
+
 
